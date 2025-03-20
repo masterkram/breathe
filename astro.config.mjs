0cbcd858a294d4333e7a8e459787d0c3de402a52
Mark Bruderer <markbruderer123@gmail.com>
2025-03-20 22:50:58 +0100

    add og image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
+  site: "https://breathe.markbruderer.com",
   vite: {
     plugins: [tailwindcss()],
   },
